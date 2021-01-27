1e3da4f152ff8928d8a305df009e2d7837da011a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-27 12:09:24 +0100

    Add example to doc

--- UnicodeTree.pm
+++ UnicodeTree.pm
@@ -120,7 +120,7 @@ Graph::Reader::UnicodeTree - Perl class for reading a graph from unicode tree te
    │   └─9
    └─10
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
  use strict;
  use warnings;
@@ -160,6 +160,31 @@ Graph::Reader::UnicodeTree - Perl class for reading a graph from unicode tree te
  print $g."\n";
 
  # Output:
+ # 1-10,1-2,1-3,1-5,1-6,3-4,6-7,6-8,6-9
+
+=head1 EXAMPLE2
+
+ use strict;
+ use warnings;
+
+ use Graph::Reader::UnicodeTree;
+
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 data_file\n";
+         exit 1;
+ }
+ my $data_file = $ARGV[0];
+
+ # Reader object.
+ my $obj = Graph::Reader::UnicodeTree->new;
+
+ # Get graph from file.
+ my $g = $obj->read_graph($data_file);
+
+ # Print to output.
+ print $g."\n";
+
+ # Output like:
  # 1-10,1-2,1-3,1-5,1-6,3-4,6-7,6-8,6-9
 
 =head1 DEPENDENCIES
