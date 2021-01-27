8e7f533f6fcd2c561dcece7f37ac472b6fd5464d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-27 12:01:15 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- UnicodeTree.pm
+++ UnicodeTree.pm
@@ -194,8 +194,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © Michal Josef Špaček 2013-2015
- BSD 2-Clause License
+© Michal Josef Špaček 2013-2015
+
+BSD 2-Clause License
 
 =head1 VERSION
 
